08786c18f0c7d162556c8db62bde96c4db91b5b7
Artem Kulikov <chigar2010@yandex.ru>
2024-10-04 21:51:07 +0200

    last fix

--- exhibition_of_kittens/api/filters.py
+++ exhibition_of_kittens/api/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Case, When, Q, Value, IntegerField
 
 from kittens.models import Breed, Kitten
 
@@ -8,7 +9,24 @@ class KittenFilter(django_filters.FilterSet):
         queryset=Breed.objects.all(), to_field_name='slug',
         field_name='breed__slug'
     )
+    breed_name = django_filters.CharFilter(method='filter_breed_name')
 
     class Meta:
         model = Kitten
         fields = ['breed',]
+
+    def filter_breed_name(self, kittens, name, value):
+        return kittens.filter(
+            Q(breed__name__icontains=value.lower()) |
+            Q(breed__name__istartswith=value.capitalize())
+        ).annotate(
+            sort_by=Case(
+                When(
+                    Q(breed__name__istartswith=value.capitalize()),
+                    then=Value(1)
+                ),
+                When(Q(breed__name__icontains=value.lower()), then=Value(2)),
+                default=Value(3),
+                output_field=IntegerField(),
+            )
+        ).order_by('sort_by')
